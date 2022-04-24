bf6a29ed1a70dd5b8cde585be0077e79c2fe5662
Fluvian <Fluvian@ProtonMail.com>
2022-04-24 03:36:28 +0200

    Label audio subsystem.

---
+++ src/boot/audio/seq.c
@@ -0,0 +1,103 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B080.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B0C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B15C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B1D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B2A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B40C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B454.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B47C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B4AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B50C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B5A8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B628.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B6B0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B770.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B7E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B91C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B96C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003B9D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BA94.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BB44.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BB78.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BBAC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BD1C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BD78.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BDF4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BF24.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003BF98.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C030.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C0EC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C108.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C1B0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C290.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C328.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C390.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C400.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C490.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C4D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C5A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C5E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C6A0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C6DC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C79C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C7B8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C7D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C7FC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C888.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C93C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C96C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003C9F4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003CA9C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/boot/audio/seq/func_8003CB94.s")
